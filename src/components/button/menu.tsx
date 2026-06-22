407265f7d80ef831802eb31b299aa34d68796dcf
jameshsu1125 <jameshsu1125@gmail.com>
2026-06-22 13:00:30 +0800

    feat: 更新菜單圖示路徑，移除前綴斜線以修正資源加載問題

--- src/components/button/menu.tsx
+++ src/components/button/menu.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 import './menu.less';
 
 const Menu = memo(({ type, menu }: { type: 'share' | 'mute'; menu?: boolean }) => {
-  useURI({ path: `/menu-icon-${type}.svg`, name: `menu-icon-${type}` });
+  useURI({ path: `menu-icon-${type}.svg`, name: `menu-icon-${type}` });
   return <div className={twMerge(type, menu && 'menu')} />;
 });
 export default Menu;
