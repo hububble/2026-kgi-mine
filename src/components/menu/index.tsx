044a054400d8e6ebc0c805541e85dc8f4e069cc5
jameshsu1125 <jameshsu1125@gmail.com>
2026-06-26 14:17:23 +0800

    feat: 整合 useClean 鉤子以處理清除操作；更新分享按鈕邏輯以支持清除功能

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -8,6 +8,9 @@ import Button from '../button';
 import Contain from '../contain';
 import TweenerProvider from '../tweenProvider';
 import './index.less';
+import useClean from '@/hooks/useClean';
+
+const needClean = true;
 
 const Menu = memo(() => {
   const [context, setContext] = useContext(Context);
@@ -15,10 +18,33 @@ const Menu = memo(() => {
 
   const [muteActive, setMuteActive] = useState(false);
 
+  const [response, clean] = useClean();
+
   useEffect(() => {
     if (muteActive) sounds?.track?.mute();
     else sounds?.track?.unmute();
   }, [muteActive]);
+
+  useEffect(() => {
+    if (response) {
+      if (response.isSuccess) {
+        setContext({
+          type: ActionType.Modal,
+          state: {
+            enabled: true,
+            title: '',
+            body: '清除成功，要重新整理頁面嗎？',
+            label: ['確定', '取消'],
+            onConfirm(label) {
+              if (label === '確定') {
+                window.location.reload();
+              }
+            },
+          },
+        });
+      }
+    }
+  }, [response]);
 
   return (
     <div className='Menu'>
@@ -26,20 +52,24 @@ const Menu = memo(() => {
         <Contain className='p-0 md:p-3.5'>
           <div className='pointer-events-auto m-1 flex flex-row items-center justify-center gap-1'>
             <Button
-              onClick={() =>
-                shareURL({
-                  onError: () => {
-                    setContext({
-                      type: ActionType.Modal,
-                      state: {
-                        enabled: true,
-                        body: '分享失敗，請使用支援 Web Share API 的瀏覽器或裝置再試一次！',
-                        label: ['確定'],
-                      },
-                    });
-                  },
-                })
-              }
+              onClick={() => {
+                if (needClean) {
+                  clean();
+                } else {
+                  shareURL({
+                    onError: () => {
+                      setContext({
+                        type: ActionType.Modal,
+                        state: {
+                          enabled: true,
+                          body: '分享失敗，請使用支援 Web Share API 的瀏覽器或裝置再試一次！',
+                          label: ['確定'],
+                        },
+                      });
+                    },
+                  });
+                }
+              }}
             >
               <Button.Menu type='share' />
             </Button>
